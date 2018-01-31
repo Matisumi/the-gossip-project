1a8eafc043536fa13fd59ab98fce43dc9e12c774
Sabine <matisuo@gmail.com>
2018-01-31 19:14:02 +0100

    clearing db

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
 
 	resources :gossips
 	root to: 'gossips#index'
-
-  #get 'gossips/new', to: 'gossips#new'
-  
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
